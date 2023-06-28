887f298e4528e2e3a0dc0b1b9f2938da3af50ede
Riley Wood <122935416+RileyWood9984@users.noreply.github.com>
2023-06-27 23:07:28 -0500

    Time: 49 ms (94.18%), Space: 42.3 MB (22.56%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let fa=[];
+    for(let i = 0; i<arr.length;i++){
+             if(fn(arr[i],i)){
+                 fa.push(arr[i])
+             }
+    }
+    return fa
+};
