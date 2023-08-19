de7915803a2de1c5cff70039f329fb557cccc1d9
Riley Wood <122935416+RileyWood9984@users.noreply.github.com>
2023-08-19 11:41:18 -0500

    Time: 67 ms (48.06%), Space: 45.6 MB (28.68%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if(root==null)return 0;
+    let r=root.right
+    let l=root.left      
+    return Math.max(maxDepth(l),maxDepth(r))+1;    
+};
