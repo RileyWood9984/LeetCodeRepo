69df2daa1aba995ddb5fc41c5cdc4e6840d7206a
Riley Wood <122935416+RileyWood9984@users.noreply.github.com>
2023-10-25 12:41:54 -0500

    Time: 113 ms (20.21%), Space: 44.9 MB (22.66%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -4,10 +4,11 @@
  */
 var removeDuplicates = function(nums) {
     nums.sort((a,b)=>a-b)
-    for(let i=0;i<nums.length;i++){
+    for(let i=0;i<nums.length;){
         if(nums[i]==nums[i+1]){
             nums.splice(i,1);
-            i--;
+        }else{
+            i++;
         }
     }
     return nums.length;
