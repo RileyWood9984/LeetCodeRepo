96b979dea6bd83526b9040a858b8e6eecfbc8f66
Riley Wood <122935416+RileyWood9984@users.noreply.github.com>
2023-11-03 12:31:35 -0500

    Time: 57 ms (67.79%), Space: 37 MB (28.28%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cs
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cs
@@ -1,13 +1,5 @@
 public class Solution {
     public int StrStr(string haystack, string needle) {
-        for (var i = 0; i < haystack.Length - needle.Length + 1; i++)
-        {
-            if (haystack.Substring(i, needle.Length) == needle)
-            {
-                return i;
-            }
-        }
-
-        return -1;
+        return haystack.IndexOf(needle);
     }
 }
