3dd2cb4d0e00a88c4c29055e87b15745470aaec0
Riley Wood <122935416+RileyWood9984@users.noreply.github.com>
2023-12-01 13:33:06 -0600

    Time: 51 ms (83.32%), Space: 41.8 MB (94.26%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    const aBin = `0b${a}`, bBin = `0b${b}`;
+    const sum = BigInt(aBin)+BigInt(bBin);
+    return sum.toString(2)
+};
